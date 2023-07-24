8b538e5285cc3b8730e06331006cd1803ac42c5c
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-07-24 15:57:42 +0300

    Logger

---
+++ src/main/java/com/codecool/marsexploration/mapexplorer/logger/FileLogger.java
@@ -0,0 +1,33 @@
+package com.codecool.marsexploration.mapexplorer.logger;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+public class FileLogger implements Logger {
+    private final String logFile;
+
+    public FileLogger(String logFile) {
+        this.logFile = logFile;
+    }
+
+    public void logError(String message) {
+        logMessage(message, "ERROR");
+    }
+
+    private void logMessage(String message, String type) {
+        String entry = String.format("[%s] %s: %s", LocalDateTime.now(), type, message);
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(logFile, true)))) {
+            out.println(entry);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void log(String message) {
+        logMessage(message, "INFO");
+    }
+}
