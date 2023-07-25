ef22d66de443821f2ce2bae16394d592b49add9e
Astrahalora <astrahalora@gmail.com>
2023-07-25 09:54:38 +0300

    Changed RandomLandingCoor

--- src/test/java/com/codecool/marsexploration/mapexplorer/calculators/service/CoordinateCalculatorImplTest.java
+++ src/test/java/com/codecool/marsexploration/mapexplorer/calculators/service/CoordinateCalculatorImplTest.java
@@ -5,12 +5,15 @@ import com.codecool.marsexploration.mapexplorer.input.service.MapFileReaderImpl;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoader;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoaderImpl;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
+import com.codecool.marsexploration.mapexplorer.maploader.model.Map;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CoordinateCalculatorImplTest {
     private final String mapFile = "src/main/resources/exploration-0.map";
@@ -18,8 +21,25 @@ class CoordinateCalculatorImplTest {
     private final MapLoader mapLoader = new MapLoaderImpl(mapFileReader);
 
     @Test
-    void getRandomLandingCoordinate() {
+    void getRandomLandingCoordinatePicksEmptySpaceCoordinate() throws FileNotFoundException {
+        MapLoader fakeMapLoader = mock(MapLoaderImpl.class);
 
+        String[][] fakeMapRepresentation = {
+                {" ", "#", "&"},
+                {" ", "*", "%"},
+                {"#", " ", "#"}
+        };
+
+        Map fakeMap = new Map(fakeMapRepresentation, true);
+
+        when(fakeMapLoader.load(mapFile)).thenReturn(fakeMap);
+
+        CoordinateCalculator coordinateCalculator = new CoordinateCalculatorImpl(fakeMapLoader.load(mapFile));
+
+        List<Coordinate> emptyCoordinates = List.of(new Coordinate(0, 0), new Coordinate(1, 0), new Coordinate(2, 1));
+        Coordinate selectedCoordinate = coordinateCalculator.getRandomLandingCoordinate(fakeMapLoader.load(mapFile));
+
+        assertTrue(emptyCoordinates.stream().anyMatch(coordinate -> coordinate.equals(selectedCoordinate)));
     }
 
     @Test
